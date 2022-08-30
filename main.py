481a804b92f0ee5cd7afaa8aba586ade93035792
ml704457 <mandelbaumleon@gmail.com>
2022-08-30 10:40:33 +0200

    commit vers la branche tampon des modifications

--- main.py
+++ main.py
@@ -36,25 +36,43 @@ def modifierunplat():
     print("ajouter le plat a modifier")
     return
 
+def supprimerplat():
+    print("Quel plat voulez-vous supprimer ?")
+    return
 
 def remplacerunplat():
     print("quel est le plat a remplacer")
     return
 
 def menuPrincipale():
-    print("1 . Afficher les plats")
-    print("2 . Ajouter des plats")
-    print("3 . Supprimer des plats")
-    print("4 . Modifier des plats")
-    print("5 . Remplacer des plats")
-    choix = input("Du coup on fait quoi ? ")
+    print("Bonjour")
+    flag = True
+    while flag:
+        print("Que souhaitez-vous faire ? ")
+        print("1. Afficher les plats")
+        print("2. Ajouter des plats")
+        print("3. Supprimer des plats")
+        print("4. Modifier des plats")
+        print("5. Remplacer des plats")
+        print("q. Quitter")
+        choix = input()
     match choix :
         case "1":
             print("Afficher les plats")
             return menuPrincipale()
+        case "2":
+            return ajouterunplat()
+        case "3":
+            return supprimerplat()
+        case "4":
+            return modifierunplat()
+        case "5":
+            return remplacerunplat()
+        case "q":
+            flag = False
         case _:
-            print("On à pas ajouter les autres")
-            return menuPrincipale()
+            print("Erreur : Commande incorrecte")
+    print("Aurevoir")
 
 
 
